fa17776ec8c81737780a9d19a14900e3f573db49
Vigneshwaran S <vickywaran312@gmail.com>
2025-10-22 20:34:27 +0530

    commit the changes in the end to end streaming project,first commit

---
+++ config/config.py
@@ -0,0 +1,6 @@
+Configuration = {
+    'AWS_ACCESS_Key': '............',
+    'AWS_SECRET_Key': '............',
+    'S3_BUCKET': 'bucket_name',
+    'REGION': 'ap-south-1'
+}
---
+++ main.py
@@ -0,0 +1,92 @@
+from pyspark.sql import SparkSession
+from config.config import Configuration
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
+from udf_utils import *
+from pyspark.sql.functions import udf
+
+
+def define_udfs():
+    return {
+        'extract_file_name_udf': udf(extract_file_name, StringType()),
+        'extract_position_udf': udf(extract_position, StringType()),
+        'extract_salary_udf': udf(extract_salary, StructType([
+            StructField('salary_start', DoubleType(), True),
+            StructField('salary_end', DoubleType(), True),
+        ])),
+        'extract_startdate_udf': udf(extract_start_date, DateType()),
+        'extract_enddate_udf': udf(extract_end_date, DateType()),
+        'extract_classcode_udf': udf(extract_class_code, StringType()),
+        'extract_requirements_udf': udf(extract_requirements, StringType()),
+        'extract_notes_udf': udf(extract_notes, StringType()),
+        'extract_duties_udf': udf(extract_duties, StringType()),
+        'extract_selection_udf': udf(extract_selection, StringType()),
+        'extract_experience_udf': udf(extract_experience_length, StringType()),
+        'extract_education_length_udf': udf(extract_education, StringType()),
+        'extract_application_length_udf': udf(extract_application_lenght, StringType()),
+    }
+
+
+if __name__ == "__main__":
+    spark = (
+        SparkSession.builder.appName('Aws_Spark_Unstructured')
+        .config(
+            'spark.jars.packages',
+            'org.apache.hadoop:hadoop-aws:3.3.1,'
+            'com.amazonaws:aws-java-sdk:1.11.469'
+        )
+        .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
+        .config('spark.hadoop.fs.s3a.access.key', Configuration.get('AWS_ACCESS_Key'))
+        .config('spark.hadoop.fs.s3a.secret.key', Configuration.get('AWS_SECRET_Key'
+        ))
+        .config(
+            'spark.hadoop.fs.s3a.aws.credentials.provider',
+            'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider'
+        )
+        .getOrCreate()
+    )
+
+    # Input directories
+    text_input_dir = "file:///C:/Users/admin/OneDrive/Documents/Data-Engineering-Projects/End-to-End-Realtime-Streaming-with-Unstructured-Data/Datasets/input_text"
+    csv_input_dir = "file:///C:/Users/admin/OneDrive/Documents/Data-Engineering-Projects/End-to-End-Realtime-Streaming-with-Unstructured-Data/Datasets/input_csv"
+    json_input_dir = "file:///C:/Users/admin/OneDrive/Documents/Data-Engineering-Projects/End-to-End-Realtime-Streaming-with-Unstructured-Data/Datasets/input_json"
+
+    # Schema definition
+    data_schema = StructType([
+        StructField('file_name', StringType(), True),
+        StructField('position', StringType(), True),
+        StructField('classcode', StringType(), True),
+        StructField('salary_start', DoubleType(), True),
+        StructField('salary_end', DoubleType(), True),
+        StructField('start_date', DateType(), True),
+        StructField('end_date', DateType(), True),
+        StructField('req', StringType(), True),
+        StructField('notes', StringType(), True),
+        StructField('duties', StringType(), True),
+        StructField('experience_length', StringType(), True),
+        StructField('selection', StringType(), True),
+        StructField('education_length', StringType(), True),
+        StructField('school_type', StringType(), True),
+        StructField('application_location', StringType(), True),
+    ])
+
+    # Load UDFs
+    udfs = define_udfs()
+
+    # Streaming read
+    job_bulletins_df = (
+        spark.readStream
+        .format('text')
+        .option('wholetext', 'true')
+        .load(text_input_dir)
+    )
+
+    # Output to console
+    query = (
+        job_bulletins_df
+        .writeStream
+        .outputMode('append')
+        .format('console')
+        .start()
+    )
+
+    query.awaitTermination()
